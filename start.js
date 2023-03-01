991eb97c52f86ec269df7760a3de111de01519e3
Dmitry <jagotti@yandex.ru>
2023-03-01 15:34:27 +0300

    add style

--- start.js
+++ start.js
@@ -6,6 +6,7 @@ function renderStartPage(container) {
 
     const text = document.createElement('h1');
     text.classList.add('choiceForm__text');
+    text.textContent = 'Выбери сложность';
     choiceForm.appendChild(text);
 
     const levelBox = document.createElement('div');
@@ -13,20 +14,25 @@ function renderStartPage(container) {
     choiceForm.appendChild(levelBox);
 
     const levelOne = document.createElement('button');
-    levelOne.classList.add('choiceForm__level');
+    levelOne.classList.add('choiceForm__level-one');
+    levelOne.textContent = '1';
     levelBox.appendChild(levelOne);
 
     const levelTwo = document.createElement('button');
-    levelTwo.classList.add('choiceForm__level');
+    levelTwo.classList.add('choiceForm__level-two');
+    levelTwo.textContent = '2';
     levelBox.appendChild(levelTwo);
 
     const levelThree = document.createElement('button');
-    levelThree.classList.add('choiceForm__level');
+    levelThree.classList.add('choiceForm__level-three');
+    levelThree.textContent = '3';
     levelBox.appendChild(levelThree);
 
-
+    choiceForm.appendChild(levelBox);
+    
     const start = document.createElement('button');
-    text.classList.add('choiceForm__button');
+    start.classList.add('choiceForm__button');
+    start.textContent = 'Старт'
     choiceForm.appendChild(start);
 
     container.appendChild(choiceForm);
