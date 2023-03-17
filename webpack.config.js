e5d16298e6e29d06dd66f7765d3506af4a121139
Dmitry <jagotti@yandex.ru>
2023-03-17 11:53:35 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
     entry: './start.js',
+    mode: 'production',
+    oninput: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resourse',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resourse',
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'static', to: 'static' }],
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
 }
